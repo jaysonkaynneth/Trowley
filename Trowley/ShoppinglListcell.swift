7382fecbbce8da0bb9030f1e93c6d8865f04f714
ganesheb <yessy.sudirgo@gmail.com>
2022-06-19 00:30:24 +0700

    customize tableviewcell xib and connect the outlets

---
+++ Trowley/ShoppinglListcell.swift
@@ -0,0 +1,27 @@
+//
+//  ShoppinglListcell.swift
+//  Trowley
+//
+//  Created by Ganesh Ekatata Buana on 17/06/22.
+//
+
+import UIKit
+
+class ShoppinglListcell: UITableViewCell {
+
+    @IBOutlet weak var listName: UILabel!
+    @IBOutlet weak var listQuantity: UILabel!
+    @IBOutlet weak var checkImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
