48ede894f8ffcdd92e18f835fe5c555d9ad2ab53
ganesheb <yessy.sudirgo@gmail.com>
2022-06-19 03:30:47 +0700

    conect shopping list tableview cells into shopping list tableview

--- Trowley/shoppingListViewController.swift
+++ Trowley/shoppingListViewController.swift
@@ -7,18 +7,29 @@
 
 import UIKit
 
-class shoppingListViewController: UIViewController {
+class shoppingListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "listCell") as? ShoppinglListcell
+        cell?.listName.text = "Nama Barang"
+        cell?.listQuantity.text = "Qty"
+        return cell ?? UITableViewCell()
+    }
 
     @IBOutlet weak var listTabBarItem: UITabBarItem!
     @IBOutlet weak var shopListLabel: UILabel!
     @IBOutlet weak var shopHistoryLabel: UILabel!
+    @IBOutlet weak var listTable: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         listTabBarItem.image = UIImage(systemName: "list.bullet.rectangle.portrait")
         listTabBarItem.selectedImage = UIImage(systemName: "list.bullet.rectangle.portrait.fill")
-
         
         shopListLabel.font = .rounded(ofSize: 32, weight: .bold)
         shopListLabel.text = "List"
@@ -26,7 +37,9 @@ class shoppingListViewController: UIViewController {
         shopHistoryLabel.font = .rounded(ofSize: 32, weight: .bold)
         shopHistoryLabel.text = "History"
         
-
+        listTable.delegate = self
+        listTable.dataSource = self
+        self.listTable.register(UINib(nibName: "ShoppinglListcell", bundle: nil), forCellReuseIdentifier: "listCell")
     
 
     }
