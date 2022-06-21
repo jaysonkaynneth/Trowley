406b72e17395bb9ecfeb6e9e2b19ec37dfce6f68
Jason Kenneth <jason.hauw@binus.ac.id>
2022-06-21 15:17:05 +0700

    making tableview

---
+++ Trowley/PantryCell.swift
@@ -0,0 +1,23 @@
+//
+//  PantryCell.swift
+//  Trowley
+//
+//  Created by Jason Kenneth on 21/06/22.
+//
+
+import UIKit
+
+class PantryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
