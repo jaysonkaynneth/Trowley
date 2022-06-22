e0a2a3fd7795ca9810134ac7631403b83e7192ad
Jason Kenneth <jason.hauw@binus.ac.id>
2022-06-22 14:42:22 +0700

    added trowley model

---
+++ Trowley/Model/Food+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Food+CoreDataClass.swift
+//  Trowley
+//
+//  Created by Jason Kenneth on 22/06/22.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class Food: NSManagedObject {
+
+}
